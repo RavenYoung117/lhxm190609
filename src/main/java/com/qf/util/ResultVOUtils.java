40354ccda58459b697d7a9abeb1d8c83c1fa146c
jokerhua <13026283899@163.com>
2019-10-16 07:51:19 +0800

    工具类补充上传

---
+++ src/main/java/com/qf/util/ResultVOUtils.java
@@ -0,0 +1,27 @@
+package com.qf.util;
+
+import com.qf.vo.ResultVo;
+
+/**
+ * 2019/10/14
+ * Administrator
+ * 处理返回的数据格式
+ */
+
+public class ResultVOUtils<T> {
+    public ResultVo success(T t){
+        ResultVo resultVo = new ResultVo();
+        resultVo.setCode(1);
+        resultVo.setMsg("成功");
+        resultVo.setData(t);
+        return resultVo;
+    }
+    public ResultVo  error(){
+        ResultVo resultVo = new ResultVo();
+        resultVo.setCode(0);
+        resultVo.setMsg("失败");
+        resultVo.setData(null);
+        return resultVo;
+    }
+
+}
