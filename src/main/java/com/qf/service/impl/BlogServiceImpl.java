5674742472369b00fee1a815f4cd3e2637805760
靓仔 <hsj5683@163.com>
2019-10-23 19:12:12 +0800

    添加事务

--- src/main/java/com/qf/service/impl/BlogServiceImpl.java
+++ src/main/java/com/qf/service/impl/BlogServiceImpl.java
@@ -120,17 +120,20 @@ public class BlogServiceImpl implements BlogService {
     }
 
     @Override
+    @Transactional
     public int deletereply(int blogid) {
         int deletereply = blogMapper.deletereply(blogid);
         return deletereply;
     }
 
     @Override
+    @Transactional
     public int deletecollect(long blogid) {
         int deletecollect = blogMapper.deletecollect(blogid);
         return deletecollect;
     }
     @Override
+    @Transactional
     public int AddBlogLike(int id) {
         int i = blogMapper.AddBlogLike(id);
         return i;
